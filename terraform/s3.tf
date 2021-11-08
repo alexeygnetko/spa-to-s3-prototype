7cf2dc9c2a0b2342196a87f45893829d07c56d66
Alexey Gnetko <a.gnetko@itransition.com>
2021-11-08 21:13:50 +0200

    ci: set 'error_document' for S3 website
    'error_document' should be set to 'index.html' in Amazon S3 to route all requests to SPA entrypoint.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,14 @@
+locals {
+  bucket_name    = "spa-to-s3-prototype.example.com"
+  spa_entrypoint = "index.html"
+}
+
 resource "aws_s3_bucket" "spa-to-s3-prototype" {
-  bucket = "spa-to-s3-prototype.example.com"
+  bucket = local.bucket_name
   acl    = "public-read"
 
   website {
-    index_document = "index.html"
+    index_document = local.spa_entrypoint
+    error_document = local.spa_entrypoint
   }
 }
